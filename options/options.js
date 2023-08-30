3022c74a24102df5faeb92d1a17c2e6134132c83
Crowdbotics <team@crowdbotics.com>
2023-08-30 22:09:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "food-at-home-43156",
+  url: "https://food-at-home-43156.botics.co",
+  api: "https://food-at-home-43156.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
